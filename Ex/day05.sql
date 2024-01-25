1f512d575d020161007615b3780ef11d942564ec
clz2024.red <clz2024.red@gmail.com>
2024-01-25 15:24:43 +0900

    day05

--- Ex/day05.sql
+++ Ex/day05.sql
@@ -1,6 +1,9 @@
 /***********************************************
-* 05일차 수업   SubQuery
+* 05일차 수업
 ***********************************************/
+-- ------------------------------------------------
+# SubQuery
+-- ------------------------------------------------
 /*
 'Den' 보다 월급을 많은 사람의 이름과 월급은?
 
@@ -171,16 +174,157 @@ where (department_id, salary) in ( select department_id, max(salary)
 ;
 
 
-
 /*
-부서별 최고값 이름
-부서별 최고값 이름
-월급 최저인 직원의 월급과 이름
+가장 적게 월급을 받는 직원의 이름, 월급은?
 */
-select min(salary), first_name
+
+select min(salary)
 from employees;
 
+select  salary, 
+        first_name, 
+        hire_date, 
+        phone_number, 
+        email
+from employees
+where salary = (select min(salary)
+                from employees);
+
+
+/*
+부서별 최고월급, 이름 --> in  --> in 확장
+*/
+
+
+-- ------------------------------------------------
+# SubQuery    ANY
+-- ------------------------------------------------
+/*
+부서번호가 110인 직원의 급여 보다 큰 
+모든 직원의 이름, 급여를 출력하세요.(or연산--> 8300보다 큰)
+*/
+
+-- 월급이 15000보다 큰 직원의 이름, 급여를 출력하세요
+select  first_name,
+        salary
+from employees
+where salary > 15000
+;
+
+-- 부서번호가 110인 직원의 월급
+select  salary
+from employees
+where department_id = 110;
+
+select  first_name,
+        salary
+from employees
+where salary > any (select  salary
+                   from employees
+                   where department_id = 110)
+;
+
+
+-- ------------------------------------------------
+# SubQuery    ALL
+-- ------------------------------------------------
+/*
+부서번호가 110인 직원의 급여 보다 큰 모든 직원의 
+이름, 급여를 출력하세요.(and연산--> 12008보다 큰)
+*/
+select salary 
+from employees
+where department_id = 110;
+
+select  first_name,
+        salary
+from employees
+where salary >ALL (select salary 
+                   from employees
+                   where department_id = 110)
+;
+
+
+select *
+from employees
+where salary > 15000
+;
+
+select *
+from employees e, departments d
+;
+
+select *
+from departments;
+
+
+/*
+각 부서별로 최고급여를 받는 사원을 출력하세요
+직원아이디, 이름, 부서번호, 월급
+*/
+-- 부서별 최고월급을 구한다
+select department_id, max(salary)
+from employees
+group by department_id
+;
+
+-- 직원테이블에서 (부서번호,최고월급) 이 동시에 만족하는 직원만 구한다
+select  employee_id,
+        first_name,
+        department_id,
+        salary
+from employees
+where (department_id, salary) in (select department_id, max(salary)
+                                  from employees
+                                  group by department_id)
+;
+
+
+select *
+from employees e,  (select department_id, max(salary) maxSalary
+                    from employees
+                    group by department_id) s
+where e.department_id = s.department_id
+and e.salary = s.maxSalary
+;
 
 
 
+-- ------------------------------------------------
+# limit
+-- ------------------------------------------------
+select *
+from employees
+order by employee_id asc
+limit 0, 5
+; -- 1부터 5개
+
+select employee_id, first_name, salary
+from employees
+order by employee_id asc 
+limit 5 offset 0
+; -- 1부터 5개
+
+
+
+select *
+from employees
+order by employee_id asc
+limit 4
+; -- 처음부터 4개
+
+
+
+/*
+07년에 입사한 직원중 급여가 많은 직원중 3에서 7등의 이름 급여 입사일은? 
+*/
+select  first_name,
+        salary,
+        hire_date
+from employees
+where hire_date >= '07/01/01'
+and hire_date < '08/01/01'
+order by salary desc
+limit 2, 5  
+; -- 3부터 5개    3,4,5,6,7
 
